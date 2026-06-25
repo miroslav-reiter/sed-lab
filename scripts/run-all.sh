19081c6b38d47929d5861a442f2fdbf367591041
Miroslav Reiter <miroslav.reiter@it-academy.sk>
2026-06-25 20:47:01 +0200

    Add run all script

---
+++ scripts/run-all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+bash solutions/01-basics.sh
+bash solutions/02-printing.sh
+bash solutions/03-substitution.sh
+bash solutions/04-deleting.sh
+bash solutions/05-regex.sh
+bash solutions/06-in-place-and-scripts.sh
